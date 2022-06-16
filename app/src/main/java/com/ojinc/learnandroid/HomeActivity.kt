d6e77699dd345a64cd944492736a46bbfdc148c5
Ojerry <onotu128@gmail.com>
2022-06-16 16:18:26 +0100

    Added About us and Contact us pages(Fragments)

--- app/src/main/java/com/ojinc/learnandroid/HomeActivity.kt
+++ app/src/main/java/com/ojinc/learnandroid/HomeActivity.kt
@@ -2,10 +2,40 @@ package com.ojinc.learnandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       when(item.itemId){
+           R.id.contactUs -> replaceFragment(ContactFragment())
+           R.id.aboutUs -> replaceFragment(AboutFragment())
+       }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+
+
+
+
+
+
 }
