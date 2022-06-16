cf831a5a64eb5367d0b8f7d016d7aa5a7fa34069
Ojerry <onotu128@gmail.com>
2022-06-16 14:09:40 +0100

    Implementation of Onboarding screen complete

--- app/src/main/java/com/ojinc/learnandroid/MainActivity.kt
+++ app/src/main/java/com/ojinc/learnandroid/MainActivity.kt
@@ -1,15 +1,105 @@
 package com.ojinc.learnandroid
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import com.ojinc.learnandroid.adapter.OnBoardingViewPagerAdapter
+import com.ojinc.learnandroid.model.onBoardingData
 
 class MainActivity : AppCompatActivity() {
+
+    var onBoardingViewPagerAdapter: OnBoardingViewPagerAdapter? = null
+    var tabLayout: TabLayout? = null
+    var onBoardingViewPager : ViewPager? = null
+    var next: TextView? = null
+    var position = 0
+    var sharedPreferences: SharedPreferences? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if(restorePrefData()){
+            val i = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        supportActionBar!!.hide()
+
+
         setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tab_indicator)
+        next = findViewById(R.id.next)
+
+        val onBoardingData:MutableList<onBoardingData> = ArrayList()
+        onBoardingData.add(onBoardingData("Intense Learning", "Learn all you need to get started with android", R.drawable.le1))
+        onBoardingData.add(onBoardingData("Skyrocket Your Skills", "Boost your Ranking on any android skill test", R.drawable.le2))
+        onBoardingData.add(onBoardingData("Fix Those Bugs", "Become a problem solving machine", R.drawable.le3))
+
+        setOnBoardingViewPagerAdapter(onBoardingData)
+
+        position = onBoardingViewPager!!.currentItem
+
+        next?.setOnClickListener{
+            if(position < onBoardingData.size){
+                position++
+                onBoardingViewPager!!.currentItem = position
+            }
+            if (position == onBoardingData.size){
+                savePrefData()
+                val i = Intent(applicationContext, HomeActivity::class.java)
+                startActivity(i)
+            }
+
+        }
+
+        tabLayout!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                position = tab!!.position
+                if(tab.position == onBoardingData.size - 1) {
+                    next!!.text = getString(R.string.getStarted)
+                } else {
+                    next!!.text = getString(R.string.next)
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+
+
     }
 
-
-
+    private fun setOnBoardingViewPagerAdapter(onBoardingData: List<onBoardingData>){
+        onBoardingViewPager = findViewById(R.id.screenPager)
+        onBoardingViewPagerAdapter = OnBoardingViewPagerAdapter(this, onBoardingData)
+        onBoardingViewPager?.adapter = onBoardingViewPagerAdapter
+        tabLayout?.setupWithViewPager(onBoardingViewPager)
+    }
+    private fun savePrefData(){
+        sharedPreferences = applicationContext.getSharedPreferences("pref", Context.MODE_PRIVATE)
+        val editor :SharedPreferences.Editor = sharedPreferences!!.edit()
+        editor.putBoolean("isFirstTimeRun", true)
+        editor.apply()
+    }
+    private fun restorePrefData(): Boolean{
+        sharedPreferences = applicationContext.getSharedPreferences("pref", Context.MODE_PRIVATE)
+        return sharedPreferences!!.getBoolean("isFirstTimeRun", false)
+    }
 
 }
--- app/src/main/java/com/ojinc/learnandroid/adapter/OnBoardingViewPagerAdapter.kt
+++ app/src/main/java/com/ojinc/learnandroid/adapter/OnBoardingViewPagerAdapter.kt
@@ -1,11 +1,19 @@
 package com.ojinc.learnandroid.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.viewpager.widget.PagerAdapter
+import com.ojinc.learnandroid.R
 import com.ojinc.learnandroid.model.onBoardingData
 
-class OnBoardingViewPagerAdapter(private var context: Context, private var onBoardingDataList: List<onBoardingData>)  : PagerAdapter() {
+class OnBoardingViewPagerAdapter(
+    private var context: Context,
+    private var onBoardingDataList: List<onBoardingData>
+) : PagerAdapter() {
     override fun getCount(): Int {
         return onBoardingDataList.size
     }
@@ -14,7 +22,28 @@ class OnBoardingViewPagerAdapter(private var context: Context, private var onBoa
         return view == `object`
     }
 
-    override fun destroyItem(container: View, position: Int, `object`: Any) {
-        super.destroyItem(container, position, `object`)
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
     }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val  view:View = LayoutInflater.from(context).inflate(R.layout.onboarding_screen_layout, null);
+        val imageView: ImageView
+        val title: TextView
+        val desc : TextView
+
+        imageView = view.findViewById(R.id.imageView)
+        title = view.findViewById(R.id.theTitle)
+        desc = view.findViewById(R.id.desc)
+
+        imageView.setImageResource(onBoardingDataList[position].imageUrl)
+        title.text = onBoardingDataList[position].title
+        desc.text = onBoardingDataList[position].desc
+
+        container.addView(view)
+        return view
+    }
+
+
+
 }
